6dde1ce5a3d600b8dd8a29308060bc115d5318b3
NataliaPoletaeva <natfly799@gmail.com>
2021-11-24 20:18:25 +0500

    Linters enabled

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* eslint-disable max-classes-per-file */
-/* eslint-disable no-use-before-define */
 
 class Book {
   constructor(title, author) {
@@ -8,31 +7,6 @@ class Book {
     this.id = Math.random();
   }
 }
-
-class Library {
-  constructor() {
-    this.data = [];
-  }
-
-  addBook(book) {
-    this.data.concat(book);
-    localStorage.setItem('library', JSON.stringify(this.data));
-    Display.addToUI(book);
-  }
-
-  removeBook(id) {
-    const book = document.getElementById(id);
-    book.remove();
-    this.data = this.data.filter((bookObj) => bookObj.id !== id);
-    localStorage.setItem('library', JSON.stringify(this.data));
-  }
-}
-
-const library = new Library();
-const currentDate = document.getElementById('date');
-/* eslint-disable */
-currentDate.innerHTML = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL);
-/* eslint-enable */
 
 class Input {
   static getInput() {
@@ -60,6 +34,29 @@ class Display {
   }
 }
 
+class Library {
+  constructor() {
+    this.data = [];
+  }
+
+  addBook(book) {
+    this.data.concat(book);
+    localStorage.setItem('library', JSON.stringify(this.data));
+    Display.addToUI(book);
+  }
+
+  removeBook(id) {
+    const book = document.getElementById(id);
+    book.remove();
+    this.data = this.data.filter((bookObj) => bookObj.id !== id);
+    localStorage.setItem('library', JSON.stringify(this.data));
+  }
+}
+
+const library = new Library();
+const currentDate = document.getElementById('date');
+currentDate.innerHTML = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL);
+
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', () => {
   const book = Input.getInput();
@@ -76,7 +73,6 @@ window.onload = () => {
   library.data.forEach((book) => Display.addToUI(book));
 };
 
-// eslint-disable-next-line no-unused-vars
 function displaySection(section) {
   const sectionList = document.getElementById('list');
   const sectionForm = document.getElementById('form');
